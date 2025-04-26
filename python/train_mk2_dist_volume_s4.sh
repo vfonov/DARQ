17cd33bb82b02252dfcb214eeeedd7835557d6c7
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2025-04-25 22:19:25 -0400

    Added training scripts

---
+++ python/train_mk2_dist_volume_s4.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+#pfx2=mk2/s2/r18/0
+
+prev=mk2/s1_small_dist/r18/0/best_loss_0_8.pth
+
+pfx1=mk2/s1_small_vol_dist/r18/0
+pfx2=mk2/s2_small_vol_dist/r18/0
+pfx3=mk2/s3_small_vol_dist/r18/0
+pfx4=mk2/s4_small_vol_dist/r18/0
+
+if [[ ! -e $pfx4/final_0_8.pth ]];then
+mkdir -p $pfx4
+
+
+## finetune!
+python aqc_training.py \
+    --net r18 \
+    --workers 2 \
+    --data /fast/vfonov/registration/pp_npy_ref \
+    --load $pfx3/best_loss_0_8.pth \
+    --db qc_db_20250217_ref.sqlite3  \
+    $pfx4 \
+    --distcalc \
+    --dist \
+    --patch 192 \
+    --noise 0.1 --lut 0.1  --nu 0.5 \
+    --lin 0.1 \
+    --n_epochs 100 \
+    --folds 8 --fold 0 --batch 160 \
+    --warmup_iter 4 --lr 0.1 --clip 4.0 --warmup_lr 1e-10 \
+    --save_final --save_best
+fi
